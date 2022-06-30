cf2030763cfc0e661c541633abe602bd1a669d9d
Alex <azakotyansky@gmail.com>
2022-06-29 22:15:18 -0400

    Added lombok test code

---
+++ src/main/java/guru/springframework/model/GenerateLombokClass.java
@@ -0,0 +1,23 @@
+package guru.springframework.model;
+
+public class GenerateLombokClass {
+
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setFirstName("First");
+        user.setLastName("Last");
+        user.setEmail("user@gmail.com");
+        System.out.println(user);
+
+        User alex = new User("Alex", "Tester", "spam@gmail.com");
+        System.out.println(user);
+
+        User jack = User.builder()
+                .firstName("Jack")
+                .lastName("QA")
+                .email("jack@yahoo.com")
+                .build();
+        System.out.println(jack);
+    }
+}
